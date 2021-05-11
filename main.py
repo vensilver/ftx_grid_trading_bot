5ba4a80150e50b6a99b37a563fbd4af266faa867
Eugene Hsu <eugenehsu@livapp.com>
2021-05-12 01:45:01 +0800

    update logic

--- main.py
+++ main.py
@@ -1,118 +1,91 @@
 # coding=utf-8
-
-import ccxt
 import datetime
 import time
-import os
-import sys
 import simplejson as json
-# import random
-# import threading
+import ftx
 
 COLOR_RESET = "\033[0;0m"
 COLOR_GREEN = "\033[0;32m"
 COLOR_RED = "\033[1;31m"
 COLOR_BLUE = "\033[1;34m"
 COLOR_WHITE = "\033[1;37m"
-LOGFILE=""
+LOGFILE = ""
 
-class Oreder_Info:
+
+class Order_Info:
     def __init__(self):
-        self.done=False
-        self.side=None
-        self.id=0
+        self.done = False
+        self.side = None
+        self.id = 0
+
 
 class Grid_trader:
-    order_list=[]
-
-    def __init__(self,exchange,symbol,grid_level=0,lower_price=0.0,upper_price=0.0,amount=0):
+    def __init__(
+        self, client, symbol, grid_level=0, lower_price=0.0, upper_price=0.0, amount=0
+    ):
         self.symbol = symbol
-        self.exchange=exchange
-        self.order_min_inteval= self.exchange.markets[self.symbol]["info"]["priceIncrement"]
-        self.grid_level=grid_level
-        self.upper_price=upper_price
-        self.lower_price=lower_price
-        self.amount=amount
-        self.inteval_profit=(self.upper_price-self.lower_price) / self.grid_level
-        pass
+        self.client = client
+        market_info = self.client.get_market(self.symbol)
+        self.price_increment = market_info["priceIncrement"]
+        self.size_increment = market_info["sizeIncrement"]
+        self.ask_price = market_info["ask"]
+        self.last = market_info["last"]
+        self.bid_price = market_info["bid"]
+        self.grid_level = grid_level
+        self.upper_price = upper_price
+        self.lower_price = lower_price
+        self.amount = amount
+        self.interval_profit = (self.upper_price - self.lower_price) / self.grid_level
+        self.order_list = []
 
     def place_order_init(self):
-        #start cal level and place grid oreder
-        for i in range(self.grid_level + 1): #  n+1 lines make n grid
-            price = self.lower_price + i * self.inteval_profit
-            bid_price, ask_price = self.send_request("get_bid_ask_price")
-            order = Oreder_Info()
-            if  price < ask_price : 
-                order.id = self.send_request("place_order","buy",price)
-                log("place buy order id = " + str(order.id) + " in "+ str(price))
-            else:
-                order.id = self.send_request("place_order","sell",price)
-                log("place sell order id = " + str(order.id) + " in "+ str(price))
+        # start cal level and place grid oreder
+        for i in range(self.grid_level + 1):  #  n+1 lines make n grid
+            price = self.lower_price + i * self.interval_profit
+            order = Order_Info()
+            try:
+                if price < self.last:
+                    order.id = self.client.place_order(market=self.symbol, side="buy", price=price, size=self.amount, post_only=True)['id']
+                    log("place buy order id = " + str(order.id) + " in " + str(price))
+                else:
+                    order.id = self.client.place_order(market=self.symbol, side="sell", price=price, size=self.amount, post_only=True)['id']
+                    log("place sell order id = " + str(order.id) + " in " + str(price))
+            except:
+                log("update last price")
+                market_info = self.client.get_market(self.symbol)
+                self.last = market_info["last"]
             self.order_list.append(order)
-    
+
     def loop_job(self):
+        fill_history = client.get_fills()
+        fill_history_dict = {o["orderId"]: o for o in fill_history}
         for order in self.order_list:
-            order_info = self.send_request("get_order",order.id)
-            side = order_info["side"]
-            if order_info["status"] == "closed":
-                new_order_price = 0.0
-                old_order_id = order_info["id"]
-                bid_price, ask_price = self.send_request("get_bid_ask_price")
-                msg = side + " order id : " + str(old_order_id)+" : " + str(order_info["price"]) + " completed , put "
-                if side == "buy" :
-                    new_order_price = float(order_info["price"]) + self.inteval_profit 
-                    order.id = self.send_request("place_order","sell",new_order_price)
+            order_info = fill_history_dict.get(order.id)
+            # print(f"order.id: {order.id}")
+            if order_info:
+                side = order_info["side"]
+                old_order_id = order_info["orderId"]
+                msg = (
+                    side
+                    + " order id : "
+                    + str(old_order_id)
+                    + " : "
+                    + str(order_info["price"])
+                    + " completed , put "
+                )
+                if side == "buy":
+                    new_order_price = float(order_info["price"]) + self.interval_profit
+                    order.id = self.client.place_order(market=self.symbol, side="sell", price=new_order_price, size=self.amount, post_only=True)['id']
                     msg = msg + "sell"
                     log(msg)
                 else:
-                    new_order_price = float(order_info["price"]) - self.inteval_profit
-                    order.id = self.send_request("place_order","buy",new_order_price)
+                    new_order_price = float(order_info["price"]) - self.interval_profit
+                    order.id = self.client.place_order(market=self.symbol, side="buy", price=new_order_price, size=self.amount, post_only=True)['id']
                     msg = msg + "buy"
-                msg = msg + " order id : " + str(order.id) + " : " + str(new_order_price)
+                msg = (
+                    msg + " order id : " + str(order.id) + " : " + str(new_order_price)
+                )
                 log(msg)
-
-    def send_request(self,task,input1=None,input2=None):
-        tries = 3
-        for i in range(tries):
-            try:
-                if task == "get_bid_ask_price":
-                    ticker =self.exchange.fetch_ticker(self.symbol)
-                    return ticker["bid"],  ticker["ask"]
-
-                elif task == "get_order":
-                    return self.exchange.fetchOrder(input1)["info"]
-
-                elif task == "place_order":
-                    #send_request(self,task,input1=side,input2=price)
-                    side = input1
-                    price = input2
-                    orderid=0
-                    if side =="buy":
-                        orderid = self.exchange.create_limit_buy_order(self.symbol,self.amount,price )["info"]["id"]
-                    else:
-                        orderid = self.exchange.create_limit_sell_order(self.symbol,self.amount,price )["info"]["id"]
-                    return orderid
-
-                else:
-                    return None
-            except ccxt.NetworkError as e:
-                if i < tries - 1: # i is zero indexed
-                    log("NetworkError , try last "+str(i) +"chances" + str(e))
-                    time.sleep(0.5)
-                    continue
-                else:
-                    log(str(e))
-                    raise
-            except ccxt.ExchangeError as e:
-                if i < tries - 1: # i is zero indexed
-                    log(str(e))
-                    time.sleep(0.5)
-                    continue
-                else:
-                    log(str(e))
-                    raise
-            break
-
 def log(msg):
     timestamp = datetime.datetime.now().strftime("%b %d %Y %H:%M:%S ")
     s = "[%s] %s:%s %s" % (timestamp, COLOR_WHITE, COLOR_RESET, msg)
@@ -124,28 +97,32 @@ def log(msg):
     except:
         pass
 
+
 def read_setting():
-        with open('setting.json') as json_file:
-            return json.load(json_file)
-             
+    with open("setting.json") as json_file:
+        return json.load(json_file)
+
+
 config = read_setting()
-LOGFILE= config["LOGFILE"]
+LOGFILE = config["LOGFILE"]
 
-exchange  = ccxt.ftx({
-    'verbose': False,
-    'apiKey': config["apiKey"],
-    'secret': config["secret"],
-    'enableRateLimit': True,
-    'headers': {
-        'FTX-SUBACCOUNT': config["sub_account"],
-    },
-})
 
-exchange_markets = exchange.load_markets()
+client = ftx.FtxClient(
+    api_key=config["apiKey"],
+    api_secret=config["secret"],
+    subaccount_name=config["sub_account"],
+)
 
-main_job = Grid_trader(exchange,config["symbol"],config["grid_level"],config["lower_price"],config["upper_price"],config["amount"])
+main_job = Grid_trader(
+    client,
+    config["symbol"],
+    config["grid_level"],
+    config["lower_price"],
+    config["upper_price"],
+    config["amount"],
+)
 main_job.place_order_init()
 while True:
-    print("Loop in :",datetime.datetime.now())
+    print("Loop in :", datetime.datetime.now())
     main_job.loop_job()
     time.sleep(1)
